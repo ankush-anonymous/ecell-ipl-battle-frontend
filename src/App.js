bbb98527b8994795d577e682bd79e74666e01363
ankush-anonymous <canankushdoit@gmail.com>
2024-02-21 23:51:09 +0530

    Pges&Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from "react";
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-bold">Hello World</h1>
+      <BrowserRouter>
+        <Routes>{/* <Route path="/" element={</>} /> */}</Routes>
+      </BrowserRouter>
     </div>
   );
 }
